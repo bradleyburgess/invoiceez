040c780cf5d451993f730cfaa58d31d68bbcbf37
Bradley Burgess <bradleyburgess@gmail.com>
2025-10-26 10:32:13 +0200

    Include footer on centered layout (login & register)

--- vue-frontend/app/layouts/centered.vue
+++ vue-frontend/app/layouts/centered.vue
@@ -1,6 +1,10 @@
 <template>
-    <main class="min-h-screen flex items-center justify-center py-12">
-        <slot />
-    </main>
-
+    <div class="relative">
+        <main class="min-h-screen flex items-center justify-center py-12">
+            <slot />
+        </main>
+        <div class="absolute bottom-0 w-full">
+            <AppFooter />
+        </div>
+    </div>
 </template>
